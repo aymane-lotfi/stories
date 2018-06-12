fa140844f435eaf82e2f5b419c2830a312d7ef10
aymane lotfi <31337952+aymane-lotfi@users.noreply.github.com>
2018-06-12 22:57:27 +0000

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
+      :aws_access_key_id      => ENV['AKIAI7AEDQ4N5VNDIUDA'],
+      :aws_secret_access_key  => ENV['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']
     }
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = ENV['aymane-lotfi']
   end
 end
